cd8cca7d0e0cb76c583b05fe1fe659f3e96373ad
Alexanderpaul301 <paulalexandre30126@gmail.com>
2025-02-04 16:42:37 +0100

    commit mardi

---
+++ install/cs7630_msgs/include/cs7630_msgs/cs7630_msgs/msg/detail/trajectory__type_support.hpp
@@ -0,0 +1,31 @@
+// generated from rosidl_generator_cpp/resource/idl__type_support.hpp.em
+// with input from cs7630_msgs:msg/Trajectory.idl
+// generated code does not contain a copyright notice
+
+#ifndef CS7630_MSGS__MSG__DETAIL__TRAJECTORY__TYPE_SUPPORT_HPP_
+#define CS7630_MSGS__MSG__DETAIL__TRAJECTORY__TYPE_SUPPORT_HPP_
+
+#include "rosidl_typesupport_interface/macros.h"
+
+#include "cs7630_msgs/msg/rosidl_generator_cpp__visibility_control.hpp"
+
+#include "rosidl_typesupport_cpp/message_type_support.hpp"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+// Forward declare the get type support functions for this type.
+ROSIDL_GENERATOR_CPP_PUBLIC_cs7630_msgs
+const rosidl_message_type_support_t *
+  ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(
+  rosidl_typesupport_cpp,
+  cs7630_msgs,
+  msg,
+  Trajectory
+)();
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // CS7630_MSGS__MSG__DETAIL__TRAJECTORY__TYPE_SUPPORT_HPP_
