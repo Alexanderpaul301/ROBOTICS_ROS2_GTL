963a6c2594e37b5af2c726daa3cb5a651d2dbdf7
Alexanderpaul301 <paulalexandre30126@gmail.com>
2025-03-03 14:58:01 +0100

    Merge of the mapping_kf before the compass

--- build/ar_slam_base/build/lib/ar_slam_base/mapping_kf.py
+++ build/ar_slam_base/build/lib/ar_slam_base/mapping_kf.py
@@ -37,8 +37,8 @@ class MappingKF(RoverOdo):
         
         # Implement Kalman prediction here
         # Compute the update in the body frame and the resulting uncertainty in the body frame
-        DeltaX = iW*S
-        DeltaP = np.zeros((3,3))
+        DeltaX = iW @ S
+        DeltaP = iW @ np.eye(12)*(encoder_precision**2) @ iW.T
         self.lock.release()
         return self.predict_delta(logger,DeltaX,DeltaP)
 
@@ -47,41 +47,131 @@ class MappingKF(RoverOdo):
         # Update the state using the provided displacement, but we only need to deal with a subset of the state
         # Assumption: deltaX and deltaP are defined in the body frame and need to be rotated to account for the jacobian 
         # of the transfer function
-        # TODO
+        # ! Checked the code and it seems to be okay
         theta = self.X[2,0]
         Rtheta = np.mat([[cos(theta), -sin(theta), 0], 
                       [sin(theta),  cos(theta), 0],
-                      [         0,           0, 1]]);
-        # self.X[0:3,0] = ...
-        # self.P[0:3,0:3] = ...
+                      [         0,           0, 1]])
+
+        A = np.eye(3)
+        A[0, 2] = -sin(theta) * DeltaX[0,0] - cos(theta) * DeltaX[1,0]
+        A[1, 2] = cos(theta) * DeltaX[0,0] - sin(theta) * DeltaX[1,0]
+
+        Q=np.eye(3)*1e-4
+
+        self.X[0:3] = self.X[0:3] + Rtheta @ DeltaX
+        self.P[0:3,0:3] = A @ self.P[0:3,0:3] @ A.T + Rtheta @ DeltaP @ Rtheta.T + Q
+
         self.lock.release()
         return (self.X,self.P)
 
 
     def update_ar(self, logger, Z, id, uncertainty):
-        # Z is a dictionary of id->vstack([x,y])
         self.lock.acquire()
+        
+        R = np.mat(np.diag([uncertainty,uncertainty]))
+        theta=self.X[2,0]
+        Rtheta=np.mat([[cos(theta),-sin(theta)],[sin(theta),cos(theta)]])
+
         # TODO
         logger.info("Update: Z="+str(Z.T)+" X="+str(self.X.T)+" Id="+str(id))
         print("Update: Z="+str(Z.T)+" X="+str(self.X.T)+" Id="+str(id))
+
+        if id in self.idx:
+            # Known landmark, we can run the KF update
+            j=self.idx[id]
+
+        else:
+            j=len(self.X)
+            self.idx[id]=j
+        # ! Initiliaze()
+            # ! Adding the new landmark to the X vector
+            Ln=self.X[0:2] + Rtheta @ Z
+            logger.info("taille Ln" + str(Ln.shape))
+            self.X=np.vstack((self.X,Ln))
+
+            # ! Updating the covariance
+            Pr=self.P[0:3,0:3]
+            
+            Hx= np.zeros((2,3)) # ! dim(Hx)=(2,3)
+            Hx[0,0]=1
+            Hx[1,1]=1
+            Hx[0, 2] = -sin(theta) * Z[0,0] - cos(theta) * Z[1,0]
+            Hx[1, 2] = cos(theta) * Z[0,0] - sin(theta) * Z[1,0]
+
+            Hz=Rtheta
+
+            Pn=Hx @ Pr @ Hx.T + Hz @ R @ Hz.T
+
+            Pnew = np.zeros((self.P.shape[0] + Pn.shape[0], self.P.shape[1] + Pn.shape[1]))
+            # Copy the covariance
+            Pnew[0:self.P.shape[0], 0:self.P.shape[1]] = self.P
+
+            # Add new covariance block
+            Pnew[self.P.shape[0]:, self.P.shape[1]:] = Pn
+            self.P = Pnew
+
+
+    # ! Update(j) Updating the data we have with the information 
         # Update the full state self.X and self.P based on landmark id
         # be careful that this might be the first time that id is observed
-        # TODO
-        # self.X = ...
-        # self.P = ...
+        
+
+        h_x=Rtheta.T @ (self.X[j:j+2]-self.X[0:2])
+
+        H=np.zeros((2,self.X.shape[0]))
+
+        delta_x=self.X[j,0]-self.X[0,0]
+        delta_y=self.X[j+1,0]-self.X[1,0]
+
+        H[0:2,0:3] = np.mat([[-cos(theta), sin(theta), -sin(theta) * delta_x - cos(theta) * delta_y],
+                    [ -sin(theta), -cos(theta),  -cos(theta) * delta_x - sin(theta) * delta_y]])
+
+        H[0:2,j:j+2]=Rtheta
+        logger.info("taille H" + str(H.shape))
+        # ! Kalman Gain
+        K = self.P @ H.T @ inv(H @ self.P @ H.T + R)
+
+        # ! Evolution of state
+        logger.info("taille X" + str(self.X.shape))
+        logger.info("taille K"+ str(K.shape))
+
+        self.X = self.X.copy() + K @ (Z - h_x)
+        
+        # ! Covariance
+        self.P = (np.eye(self.P.shape[0]) - K @ H) @ self.P
+
         self.lock.release()
         return (self.X,self.P)
 
     def update_compass(self, logger, Z, uncertainty):
         self.lock.acquire()
-        # TODO
-        logger.info("Update: S="+str(Z)+" X="+str(self.X.T))
-        # Update the full state self.X and self.P based on compass measurement
-        # TODO
-        # self.X = ...
-        # self.P = ...
+
+        logger.info("Update: Z=" + str(Z) + " X=" + str(self.X.T))
+        
+        H= np.zeros((1,len(self.X)))
+        H[0,2] = 1  
+        
+        # Innovation (différence entre la mesure et l'orientation prédite)
+        innovation = np.mat([[(Z - self.X[2, 0] + pi) % (2 * pi) - pi]])  # Normaliser à [-pi, pi]
+        
+        # Calcul de la covariance de l'innovation S (incertitude de la mesure du compas)
+        S = H @ self.P @ H.T + uncertainty  
+        K = self.P @ H.T @ inv(S)  # Gain de Kalman
+        
+        # Mise à jour de l'estimation de l'état (X) avec l'innovation
+        self.X = self.X.copy() + K @ innovation
+        
+        # Normalisation de theta pour qu'il soit dans l'intervalle [-pi, pi]
+        self.X[2, 0] = (self.X[2, 0] + pi) % (2 * pi) - pi  # Assurer que theta reste dans la plage
+        
+        # Mise à jour de la matrice de covariance (P)
+        self.P = (np.eye(self.P.shape[0]) - K @ H) @ self.P
+
         self.lock.release()
-        return (self.X,self.P)
+        return self.X, self.P
+
+
 
 
     def publish(self, pose_pub, odom_pub, target_frame, stamp, child_frame):
